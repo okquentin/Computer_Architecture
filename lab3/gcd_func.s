eb0473694a3bf5dea30b59ff7c3d54f0a3c14cbf
okquentin <qosterhage@outlook.com>
2024-09-23 17:28:09 -0400

    Documentation for gcd and greet

--- lab3/gcd_func.s
+++ lab3/gcd_func.s
@@ -5,25 +5,25 @@
 # according to the Euclidean algorithm
 
 main:
-    addi a0, zero, 48 # Initalize parameters with constant
+    addi a0, zero, 48 # initalize parameters with constants
     addi a1, zero, 64
-    jal ra, gcd 
-    jal x0, done
+    jal ra, gcd # function call
+    jal x0, done # jump to done, exit program
 
 gcd:
-    beq a0, a1, return
-    blt a0, a1, a1bigger
-    sub t0, a0, a1
-    add a0, x0, t0
-    jal x0, gcd
+    beq a0, a1, return # if a=b, end loop; a=b=GCD
+    blt a0, a1, a1bigger # check if b>a
+    sub t0, a0, a1 # subtract b from a since a>b
+    add a0, x0, t0 #load difference into a since a>b
+    jal x0, gcd #loop
 
 a1bigger:
-    sub t0, a1, a0
-    add a1, x0, t0
-    jal x0, gcd
+    sub t0, a1, a0 # subtract a from b since b>a
+    add a1, x0, t0 # load difference into b since b>a
+    jal x0, gcd #loop
 
 return:
-    jr ra
+    jr ra # return to main
 
 done:
     add x0, x0, x0
--- lab3/greet_func.s
+++ lab3/greet_func.s
@@ -3,19 +3,22 @@
 # September 2024
 # Description: considering N people in a room, suppose each person can greet each other only once.
 # This function calculates how many greetings would occur
+# Essentially calculates Σ(i=1 to N) of N-1 
+
 main:
-    addi a0, x0, 4 # Define a0 = N
-    jal x1, greet_calc
-    jal x0, done
+    addi a0, x0, 4 # define a0 = N
+    jal x1, greet_calc # function call, x1 = ra
+    jal x0, done # jump to done, exit program
 
 greet_calc:
-    addi a0, a0, -1
-    beq a0, x0, return
-    add t0, t0, a0
-    jal x0, greet_calc
+    addi a0, a0, -1 # decrement N
+    beq a0, x0, return # check if N is zero, if so exit the loop
+    add t0, t0, a0 # use t0 to save the sum
+    jal x0, greet_calc # loop until N is zero
 
 return:
-    add a0, x0, t0
+    add a0, x0, t0 # set a0 equal to sum
+    jr ra # return to main
 
 done:
     add x0, x0, x0
